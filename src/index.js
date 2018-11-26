32ea40b1522067e5e815a274c7afdb8b35e01b52
Rory <wrk961@gmail.com>
2018-11-25 22:18:41 -0500

    Add scheduled upload and nmap

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import AWS from 'aws-sdk';
 const Bucket = 'compression-space-transport';
 const StatusPrefix = 'status-files';
 
-const targets = [
+const autoUploadTargets = [
 	{ localPath: '/var/lib/dhcpd/dhcpd.leases', key: `${StatusPrefix}/dhcpd.leases` },
 	{ localPath: '/var/log/system-data.json', key: `${StatusPrefix}/system-data.json` },
 	{ localPath: '/var/log/iptstate.txt', key: `${StatusPrefix}/iptstate.txt` },
 
+	{ localPath: '/var/log/nmap.json', key: `${StatusPrefix}/nmap.json` },
+
 	{ localPath: '/lib/systemd/system/config-copy.service', key: `${StatusPrefix}/config-copy.service` },
 	{ localPath: '/lib/systemd/system/tail2s3.service', key: `${StatusPrefix}/tail2s3.service` },
 	{ localPath: '/lib/systemd/system/stats-generator.service', key: `${StatusPrefix}/stats-generator.service` },
+];
+
+const scheduledUploadTargets = [
+	{ localPath: '/var/log/messages', key: `${StatusPrefix}/messages`, waitMs: 15000 },
 ];
 
 const s3 = new AWS.S3();
@@ -35,7 +41,15 @@ function autoUpload({ localPath, key }) {
 	});	
 }
 
+function scheduledUpload({ localPath, key, waitMs }) {
+	setInterval(() => {
+		console.log('Updating file', localPath);
+		upload({ localPath, key });
+	}, waitMs);
+}
+
 function main() {
-	targets.forEach(autoUpload);
+	autoUploadTargets.forEach(autoUpload);
+	scheduledUploadTargets.forEach(scheduledUpload);
 }
 main();
